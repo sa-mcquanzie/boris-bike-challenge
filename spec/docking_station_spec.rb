b778c51e2b0d4693fd8239b375f05422decda5f5
danroche10 <danielroche10@gmail.com>
2021-07-27 18:02:28 +0100

    completed step 8 adding relevant test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,18 +4,15 @@ describe 'DockingStation' do
   before(:each) do 
     @docking_station = DockingStation.new 
   end
-
-  it "successfully returns new DockingStation object" do
-    # expect(DockingStation.new_method).to eq "hello world"
-  end
-
+  
   it 'responds to release_bike' do
     expect(@docking_station).to respond_to('release_bike')
   end
 
-  it 'responds to release_bike' do
-    expect(@docking_station.release_bike).to eq("Hello, World!")
-  end
+  # one liner version of the above test
+  subject { @docking_station }
+  it { is_expected.to respond_to 'release_bike' }
+
 end
 
 
