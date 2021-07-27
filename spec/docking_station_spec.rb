0fabc9af0c8dec95d42c755fa1f5934dcba3c4f1
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-27 17:47:55 +0100

    Added respond_to release_bike test to docking station spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,6 +10,10 @@ describe 'DockingStation' do
   end
 
   it 'responds to release_bike' do
+    expect(@docking_station).to respond_to('release_bike')
+  end
+
+  it 'responds to release_bike' do
     expect(@docking_station.release_bike).to eq("Hello, World!")
   end
 end
