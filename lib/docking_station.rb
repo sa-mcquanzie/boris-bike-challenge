6ac74368c9889feaf25738368fe05f6abe6b48f9
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-07-28 16:54:14 +0100

    progress on step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,25 @@ class DockingStation
     @bikes = []
   end
 
+  def dock_empty?
+    @bikes.empty?
+  end
+
   def release_bike
-    raise "No bikes are available" if @bikes.empty?
-    @bikes.last
+    if dock_empty? 
+      raise "No bikes are available" 
+    else @bikes.last
+    end
+  end
+
+  def dock_full?
+    @bikes.size >= 20
   end
 
   def dock bike
-    raise "Docking station is full" if @bikes.size >= 20
-    @bikes << bike
+   if dock_full? 
+    raise "Docking station is full" 
+   else @bikes << bike
+   end
   end
 end
