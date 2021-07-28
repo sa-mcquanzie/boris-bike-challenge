4009a880c1007eb1f1ec381767c7e733437c7644
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-28 15:20:12 +0100

    Completed step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,17 @@
 require "./lib/bike.rb"
 
 class DockingStation
+  attr_reader :bikes
+
   def initialize
-    
+    @bikes = []
   end
 
   def release_bike
     Bike.new
+  end
+
+  def dock
+
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,12 +10,20 @@ describe DockingStation do
     expect(@docking_station).to respond_to('release_bike')
   end
 
+  it 'responds to dock' do
+    expect(@docking_station).to respond_to('dock')
+  end
+
   it "should get a bike" do
     expect(@docking_station.release_bike.instance_of?(Bike)).to eq(true)
   end
 
   it "should get a working bike" do
     expect(@docking_station.release_bike.working?).to eq(true)
+  end
+
+  it 'should return an array of docked bikes' do
+    expect(@docking_station.bikes.instance_of?(Array)).to eq(true)
   end
 
 
